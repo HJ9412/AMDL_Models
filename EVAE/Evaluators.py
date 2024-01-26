1a4b32a24ae7b947fc5e4c0cf4ca080d394da867
HJ9412 <153887806+HJ9412@users.noreply.github.com>
2024-01-26 16:53:03 +0900

    Update Evaluators.py

--- EVAE/Evaluators.py
+++ EVAE/Evaluators.py
@@ -50,8 +50,8 @@ class Evaluator3(nn.Module):    # Bias Eraser
         # Layers
         self.fc_x1h1 = nn.Linear(data_dim, hidden_dim1)
         self.fc_x2h2 = nn.Linear(data_dim, hidden_dim1)
-        self.fc_hA = nn.Linear(hidden_dim1, hidden_dim2)
-        self.fc_hB = nn.Linear(hidden_dim1, hidden_dim2)
+        self.fc_sA = nn.Linear(hidden_dim1, hidden_dim2)
+        self.fc_sB = nn.Linear(hidden_dim1, hidden_dim2)
         self.fc_eval = nn.Linear(hidden_dim2, value_dim)
 
     def forward(self, x1, x2):
@@ -59,9 +59,9 @@ class Evaluator3(nn.Module):    # Bias Eraser
         x2 = torch.flatten(x2, start_dim = 1)
         h1 = F.relu(self.fc_x1h1(x1))               # Layers x1h1, x2h2 makes the evaluation a noncommutative operation
         h2 = F.relu(self.fc_x2h2(x2))
-        sA = F.relu(self.fc_hA(h1)+self.fc_hA(h2))  # Applying common layers to h1 and h2 produces intermingled states hA and hB
-        sB = F.relu(self.fc_hB(h1)+self.fc_hB(h2))
-        values = F.relu(self.fc_eval(sA+sB))        # The last layer even intermingles the two states hA and hB to make it completely forget its pathway
+        sA = F.relu(self.fc_hA(h1)+self.fc_sA(h2))  # Applying common layers to h1 and h2 produces intermingled states sA and sB
+        sB = F.relu(self.fc_sB(h1)+self.fc_sB(h2))
+        values = F.relu(self.fc_eval(sA+sB))        # The last layer even intermingles the two states sA and sB to make it completely forget its pathway
         return values
 
 class Evaluator4(nn.Module):    # Non-biased Feature Extractor
@@ -97,25 +97,29 @@ class Evaluator5(nn.Module):    # Non-biased Feature Extractor 2
     def __init__(self, data_dim, hidden_dim1, hidden_dim2, value_dim):
         super(Evaluator, self).__init__()
         # Layers
+        self.fc_x0h0 = nn.Linear(data_dim, hidden_dim1)
         self.fc_x1h1 = nn.Linear(data_dim, hidden_dim1)
-        self.fc_x2h2 = nn.Linear(data_dim, hidden_dim1)
-        self.fc_hA = nn.Linear(hidden_dim1, hidden_dim1)
-        self.fc_hB = nn.Linear(hidden_dim1, hidden_dim1)
+        self.fc_h0 = nn.Linear(hidden_dim1, hidden_dim1)
+        self.fc_h0 = nn.Linear(hidden_dim1, hidden_dim1)
         self.fc_state = nn.Linear(hidden_dim1, hidden_dim2)
-        self.fc_eval = nn.Linear(hidden_dim2*2, value_dim)
+        self.fc_eval = nn.Linear(hidden_dim2*4, value_dim)
 
-    def forward(self, x1, x2):
+    def forward(self, x0, x1):
+        x0 = torch.flatten(x0, start_dim = 1)
         x1 = torch.flatten(x1, start_dim = 1)
-        x2 = torch.flatten(x2, start_dim = 1)
+        h0 = F.relu(self.fc_x0h0(x0))
         h1 = F.relu(self.fc_x1h1(x1))
-        h2 = F.relu(self.fc_x2h2(x2))
-        h1A = self.fc_hA(h1)
-        h2A = self.fc_hA(h2)
-        hA = F.relu(h1A + h2A)
-        h1B = self.fc_hB(h1)
-        h2B = self.fc_hB(h2)
-        hB = F.relu(h1B - h2B)
+        h00 = self.fc_h0(h0)
+        h10 = self.fc_h0(h1)
+        h01 = self.fc_h1(h0)
+        h11 = self.fc_h1(h1)      # hij = fc_hj(hi). We want to erase i from it.
+        hA = F.relu(h00 + h11)
+        hB = F.relu(h10 + h01)
+        hC = F.relu(h00 - h11)    # {h00, h10, h01, h11} to {h00+h11, h10+h01, h00-h11, h10-h01}
+        hD = F.relu(h10 - h01)    # is a transformation of four vectors into non-factorable combinations.
         sA = F.relu(self.fc_state(hA))
         sB = F.relu(self.fc_state(hB))
-        values = F.relu(self.fc_eval(torch.concat((sA,sB), dim=1)))
+        sC = F.relu(self.fc_state(hC))
+        sD = F.relu(self.fc_state(hD))
+        values = F.relu(self.fc_eval(torch.concat((sA, sB, sC, sD), dim=1)))
         return values
